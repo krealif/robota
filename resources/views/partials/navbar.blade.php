e6ec84cbea0078cf833620f05a725e4809601f1c
Shavana Alif <85614368+krealif@users.noreply.github.com>
2022-10-24 21:33:09 +0700

    initial commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar bg-white navbar-expand-sm sticky-top">
+    <div class="container-xl">
+        <a class="navbar-brand d-inline-flex align-items-center" href="#">
+            <img src="assets/images/site/robota-logo.svg" alt="Robota Logo" height="16">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item py-2 py-sm-0">
+                <a href="#" class="btn btn-primary ms-sm-2 w-100 w-lg-auto">Login</a>
+              </li>
+            </ul>
+        </div>
+    </div>
+</nav>
